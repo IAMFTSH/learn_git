cf22f4ac7118f97cba6a1abe19ea88d8f290cf29
IAMFTSH <2244056760@qq.com>
2020-06-23 18:15:28 +0800

    1.写了一个Dao的接口，在Dao文件夹的Dao类中

---
+++ src/com/company/dao/Dao.java
@@ -0,0 +1,11 @@
+package com.company.dao;
+
+/**
+ * @author FTSH
+ * @date 2020/6/23 - 18:13
+ */
+public class Dao {
+    public static void main(String[] args) {
+        System.out.println("我写了一个查找校历的接口");
+    }
+}
